f233d44e8764ce380ec98d49e2d617336edd8aac
geethasamynathan <geethasamynathan2011@gmail.com>
2024-11-22 18:01:49 +0530

    useform hook example added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 import LoginForm from "./components/controlledform";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sample from "./components/sample";
-import UnControlledLoginForm from "./components/uncontrolledcomponent";
+import FormWithHook from "./components/formwithhook";
+
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<UnControlledLoginForm/>} />
+          <Route path="/" element={<FormWithHook />} />
           <Route path="/sample" element={<Sample />} />
         </Routes>
       </Router>
